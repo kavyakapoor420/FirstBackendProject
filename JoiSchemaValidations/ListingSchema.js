203c6566ab0b3f162b212270f641c01f9e493c95
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-01-23 23:34:12 +0530

    changes in review and listing Schema

--- JoiSchemaValidations/ListingSchema.js
+++ JoiSchemaValidations/ListingSchema.js
@@ -1,13 +1,58 @@
 const Joi=require('joi')
 
+// const listingSchema=Joi.object({
+//     listing:Joi.object({
+//         title:Joi.string().required(),
+//         description:Joi.string().required(),
+//         location:Joi.string().required(),
+//         price:Joi.number().required().min(0),
+//         image:Joi.string().allow('',null),// Allow empty or null it is not required compulsory
+//         country:Joi.string().required()
+//     })
+// })
+
+// module.exports=listingSchema
+
+
 const listingSchema=Joi.object({
     listing:Joi.object({
-        title:Joi.string().required(),
-        description:Joi.string().required(),
-        location:Joi.string().required(),
-        price:Joi.number().required().min(0),
-        image:Joi.string().allow('',null),// Allow empty or null it is not required compulsory
-        country:Joi.string().required()
+        title:Joi.string().required().messages({
+            "string.base":"Title should be a string",
+             "string.empty":"Title is required"
+          }),
+          description:Joi.string().required().messages({
+            "string.base":"Description should be string",
+            "string.empty":"Description is required"
+          }),
+          image:Joi.string().allow("",null),
+    
+                price:Joi.number().required().min(0).messages({
+                    "number.base":"Price must be a number",
+                    "number.empty":"Price is required"
+                }),
+                location:Joi.string().required.messages({
+                    "string.base":"location should be string",
+                    "string.empty":"location is required"
+                }),
+                country:Joi.string().required().messages({
+                    "string.base":"counrty should be a string",
+                    "string.empty":'country is required'
+                }),
+                //   category:Joi.string()
+        //      .valid(
+        //         "Trending",
+        //         "Rooms",
+        //         "Iconic Cities",
+        //         "Mountains",
+        //         "Castles",
+        //         "Amazing Pools",
+        //         "Camping",
+        //         "Farms",
+        //         "Arctic"
+        //      ).required().messages({
+        //         "any.only":"Category must be one of specified value",
+        //         "string.empty":"Category is required"
+        //      })
     })
 })
 
--- JoiSchemaValidations/ReviewSchema.js
+++ JoiSchemaValidations/ReviewSchema.js
@@ -1,9 +1,26 @@
 const Joi=require('joi')
 
+// const reviewSchema=Joi.object({
+//     review:Joi.object({
+//         rating:Joi.number().required().min(1).max(5),
+//         comment:Joi.string().required()
+//     })
+// })
+
+
+// module.exports=reviewSchema
+
+
 const reviewSchema=Joi.object({
     review:Joi.object({
-        rating:Joi.number().required().min(1).max(5),
-        comment:Joi.string().required()
+       rating:Joi.number().min(1).max(5).required().messages({
+        'number.base':'rating should be number',
+        "number.empty":"rating is required"
+       }),
+       comment:Joi.string().required().messages({
+        "string.base":"comment should be string",
+        "string.empty":'comment is required'
+       })
     })
 })
 
